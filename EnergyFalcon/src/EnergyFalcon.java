dc1ef8f4db5149f027defa3526b3734df901e3be
Brandon Johnson <brajohns@mtu.edu>
2017-01-18 20:09:56 -0500

    Initial commit with EGit.

---
+++ EnergyFalcon/src/EnergyFalcon.java
@@ -0,0 +1,30 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import arcadia.Game;
+import arcadia.Input;
+import arcadia.Sound;
+
+public class EnergyFalcon extends Game{
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public EnergyFalcon(){
+		
+	}
+	
+	@Override
+	public void tick(Graphics2D graphics, Input input, Sound sound) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Image cover() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
