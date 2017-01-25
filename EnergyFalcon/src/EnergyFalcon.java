bbc6ab6a22f8d1c2d7cddb2742a055440f3a8e5c
denu8thell <zebrahome10@gmail.com>
2017-01-25 12:59:19 -0500

    Added player class and movement
    Added a new class, the Player class, and implemented a rudimentery
    movement system.

--- EnergyFalcon/src/EnergyFalcon.java
+++ EnergyFalcon/src/EnergyFalcon.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import arcadia.Arcadia;
 import arcadia.Game;
@@ -10,17 +11,23 @@ import shooter.Shooter;
 
 public class EnergyFalcon extends Game{
 	
+	private static Player p;
+	
 	public static void main(String[] args){
-		Arcadia.display(new Arcadia(new Game[ ] {new DodgeGame(), new Shooter() }));
+		p = new Player();
+		//Test code in line below
+		Arcadia.display(new Arcadia(new Game[ ] {/*new EnergyFalcon(),*/ new DodgeGame(), new Shooter() }));
 	}
 	
 	@Override
 	public void tick(Graphics2D graphics, Input input, Sound sound) {
-		
+		p.onTick(input);
 	}
 
 	@Override
 	public Image cover() {
+		//Test code
+		//return new BufferedImage(100, 100, BufferedImage.TYPE_4BYTE_ABGR);
 		return null;
 	}
 
---
+++ EnergyFalcon/src/Player.java
@@ -0,0 +1,38 @@
+import java.awt.Graphics2D;
+
+import arcadia.Button;
+import arcadia.Input;
+
+public class Player {
+	//This sets the amount of pixels you move per tick.
+	//Obviously, a higher number is faster, but less precise for movement.
+	//If we want better movement(speed+precision), we need some sort of basic acceleration model.
+	private static final int DELTA = 4;
+	//X Y coordinates, relative to the top left of the screen.
+	private int x, y;
+	
+	public Player(){
+		x = 0;
+		y = 0;
+	}
+	//Method that should be called every tick.
+	public void onTick(Input input){
+		if(input.pressed(Button.U)){
+			y-=DELTA;
+		}
+		if(input.pressed(Button.D)){
+			y+=DELTA;
+		}
+		if(input.pressed(Button.L)){
+			x-=DELTA;
+		}
+		if(input.pressed(Button.R)){
+			x+=DELTA;
+		}
+		System.out.println("("+x+", "+y+")");
+	}
+	
+	public void draw(Graphics2D g){
+		//TODO insert drawing code for player
+	}
+}
