4db3a5101e020887ae548222c7bf991c89bb37fd
dante_000 <dante_000@141.219.244.36>
2017-01-25 18:51:14 -0500

    temp cover picture added

--- EnergyFalcon/src/EnergyFalcon.java
+++ EnergyFalcon/src/EnergyFalcon.java
@@ -1,5 +1,8 @@
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import arcadia.Arcadia;
 import arcadia.Game;
@@ -8,21 +11,31 @@ import arcadia.Sound;
 import dodge.DodgeGame;
 import shooter.Shooter;
 
-public class EnergyFalcon extends Game{
-	
-	public static void main(String[] args){
-		Arcadia.display(new Arcadia(new Game[ ] {new DodgeGame(), new Shooter() }));
+public class EnergyFalcon extends Game {
+
+	Image cover;
+
+	public EnergyFalcon() {
+
+		try {
+			cover = ImageIO.read(this.getClass().getResource("cover.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
+	public static void main(String[] args) {
+		Arcadia.display(new Arcadia(new Game[] { new EnergyFalcon(), new DodgeGame(), new Shooter() }));
+	}
+
 	@Override
 	public void tick(Graphics2D graphics, Input input, Sound sound) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Image cover() {
-		return null;
+		return cover;
 	}
-
 }
