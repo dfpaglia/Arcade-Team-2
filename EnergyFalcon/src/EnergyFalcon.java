7adcb5b90c287e9ae629d886757d56013f704bff
bamalets <bamalets@mtu.edu>
2017-01-29 13:20:08 -0500

    Woooo

--- EnergyFalcon/src/EnergyFalcon.java
+++ EnergyFalcon/src/EnergyFalcon.java
@@ -1,5 +1,9 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import arcadia.Arcadia;
 import arcadia.Game;
@@ -8,21 +12,43 @@ import arcadia.Sound;
 import dodge.DodgeGame;
 import shooter.Shooter;
 
-public class EnergyFalcon extends Game{
-	
-	public static void main(String[] args){
-		Arcadia.display(new Arcadia(new Game[ ] {new DodgeGame(), new Shooter() }));
-	}
-	
-	@Override
-	public void tick(Graphics2D graphics, Input input, Sound sound) {
-		// TODO Auto-generated method stub
+public class EnergyFalcon extends Game {
+
+	Image cover, background;
+	private static Player p;
+
+	public EnergyFalcon() {
+		
+		try {
+			cover = ImageIO.read(this.getClass().getResource("cover.jpg"));
+			background = ImageIO.read(this.getClass().getResource("Arena2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
 
-	@Override
-	public Image cover() {
-		return null;
+	public static void main(String[] args) {
+		p = new Player();
+		Arcadia.display(new Arcadia(new Game[] { new EnergyFalcon()}));
 	}
 
+	@Override
+	public void tick(Graphics2D graphics, Input input, Sound sound) {
+		graphics.setColor(Color.black);
+		graphics.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+		graphics.drawImage(background, 0, 0, null);
+		p.onTick(input);
+		p.draw(graphics);
+	}
+
+	
+	@Override
+	public Image cover() {
+		return cover;
+	}
+	
 }
