bedc82ae14b24fc6a2ea7b54fc42b985543f1e17
denu8thell <zebrahome10@gmail.com>
2017-02-03 21:56:47 -0500

    Changed movement.
    Movement is now much more smooth, but is probably more resource
    intensive. Tweaking may be necessary.

--- EnergyFalcon/src/Player.java
+++ EnergyFalcon/src/Player.java
@@ -14,14 +14,18 @@ public class Player implements Actor{
 	// If we want better movement(speed+precision), we need some sort of basic
 	// acceleration model.
 	private static final int DELTA = 4;
-	private static final int ACCEL = 4;
-	private static final int DECCEL= 4;
+	private static final double ACCEL = 1;
+	private static final double DECCEL= 0.75;
 	//This sets the player sprite dimensions. Changing these will change the size
 	//of the sprite, without having to screw with image editing programs.
 	private static final int PLAYER_WIDTH = 75;
 	private static final int PLAYER_HEIGHT = 63;
+	//Max Velocity (magnitude)
+	private static final double V_MAX = 10;
 	// X Y coordinates, relative to the top left of the screen.
 	private double x, y;
+	private Vector2D vel;
+	private boolean accelerated = false;
 	private Image playerSprite;
 	//TODO instatiate collider
 	private Collider collider;
@@ -32,6 +36,7 @@ public class Player implements Actor{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		vel = new Vector2D(0,0,1);
 		playerSprite = playerSprite.getScaledInstance(PLAYER_WIDTH, PLAYER_HEIGHT, 0);
 		x = Game.WIDTH / 2;
 		y = Game.HEIGHT / 2;
@@ -39,19 +44,50 @@ public class Player implements Actor{
 
 	// Method that should be called every tick.
 	public void onTick(Input input) {
+		Vector2D projection = new Vector2D(0,0,1);
+		accelerated = false;
+		
 		if (input.pressed(Button.U)) {
-			y -= DELTA;
+			vel = Vector2D.add(vel, new Vector2D(0, -ACCEL, 1));
+			accelerated = true;
+		}else if (input.pressed(Button.D)) {
+			vel = Vector2D.add(vel, new Vector2D(0, ACCEL, 1));
+			accelerated = true;
+		}else{
+			projection = Vector2D.add(projection, Vector2D.project(vel, new Vector2D(0, 1, 1)));
 		}
-		if (input.pressed(Button.D)) {
-			y += DELTA;
-		}
+		
 		if (input.pressed(Button.L)) {
-			x -= DELTA;
+			vel = Vector2D.add(vel, new Vector2D(-ACCEL, 0, 1));
+			accelerated = true;
+		}else if (input.pressed(Button.R)) {
+			vel = Vector2D.add(vel, new Vector2D(ACCEL, 0, 1));
+			accelerated = true;
+		}else{
+			projection = Vector2D.add(projection, Vector2D.project(vel, new Vector2D(1, 0, 1)));
 		}
-		if (input.pressed(Button.R)) {
-			x += DELTA;
+		projection = Vector2D.scale(Vector2D.unitVector(projection), DECCEL);
+		
+		if(projection.getX()*projection.getX() > vel.getX()*vel.getX()){
+			vel.setX(0);
+		}else{
+			vel.setX(vel.getX() - projection.getX());
 		}
+		
+		if(projection.getY()*projection.getY() > vel.getY()*vel.getY()){
+			vel.setY(0);
+		}else{
+			vel.setY(vel.getY() - projection.getY());
+		}
+		
+		if(vel.magnitude() > V_MAX){
+			vel = Vector2D.scale(Vector2D.unitVector(vel), V_MAX);
+		}
+		
+		x += vel.getX();
+		y += vel.getY();
 	}
+	
 	public void draw(Graphics2D g) {
 		g.drawImage(playerSprite, (int)Math.round(x - (PLAYER_WIDTH/2)), (int)Math.round(y - (PLAYER_HEIGHT/2)), null);
 	}
