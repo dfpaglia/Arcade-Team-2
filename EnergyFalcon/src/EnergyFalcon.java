98d1749f8f2717ced772b25933d000501cea8ed1
bamalets <bamalets@mtu.edu>
2017-02-05 13:10:18 -0500

    Health added and new arena

--- EnergyFalcon/src/EnergyFalcon.java
+++ EnergyFalcon/src/EnergyFalcon.java
@@ -13,42 +13,47 @@ import dodge.DodgeGame;
 import shooter.Shooter;
 
 public class EnergyFalcon extends Game {
-
 	Image cover, background;
-	private static Player p;
+	private Player p;
+	private GenericEnemy e;
+	private PlayerHealth h;
+	int hCount = 4;
 
 	public EnergyFalcon() {
+		p = new Player();
+		e = new GenericEnemy();
+		h = new PlayerHealth();
 		
 		try {
 			cover = ImageIO.read(this.getClass().getResource("cover.jpg"));
-			background = ImageIO.read(this.getClass().getResource("Arena2.png"));
+			background = ImageIO.read(this.getClass().getResource("ArenaFix.png")).getScaledInstance(Game.WIDTH, Game.HEIGHT, 0);;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
+
 	}
 
 	public static void main(String[] args) {
-		p = new Player();
-		Arcadia.display(new Arcadia(new Game[] { new EnergyFalcon()}));
+		Arcadia.display(new Arcadia(new Game[] {new EnergyFalcon()}));
 	}
 
 	@Override
 	public void tick(Graphics2D graphics, Input input, Sound sound) {
+		// TODO Auto-generated method stub
 		graphics.setColor(Color.black);
 		graphics.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
 		graphics.drawImage(background, 0, 0, null);
+		graphics.drawImage(h.healthDraw(hCount), -20, -20, null);
 		p.onTick(input);
+		e.onTick(input);
 		p.draw(graphics);
+		e.draw(graphics);
+		
 	}
 
-	
 	@Override
 	public Image cover() {
 		return cover;
 	}
-	
+
 }
