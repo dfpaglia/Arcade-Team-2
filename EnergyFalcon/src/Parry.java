d9c9aa3207ce6f315c7474a5111d4fee5777845d
jtbauer <jobauer@mtu.edu>
2017-03-26 13:52:43 -0400

    Better Delay for parry/block

--- EnergyFalcon/src/Parry.java
+++ EnergyFalcon/src/Parry.java
@@ -5,7 +5,7 @@ import arcadia.Input;
 
 public class Parry {
 	private static final long PARRY_DELTA = 500000000L;
-	private static final long PARRY_DELAY_DELTA = 30000000L;
+	private static final long PARRY_DELAY_DELTA = 850000000L;
 
 	private static final double PARRY_WIDTH = 10;
 	private static final double PARRY_LENGTH = 68;
@@ -24,16 +24,16 @@ public class Parry {
 		public ParryHitBox(double x, double y, double width, double height, Player p) {
 			super(x, y, width, height, CollisionType.PLAYER_HURTBOX_COLLISION);
 		}
-		
+
 		@Override
 		void onCollide(CollisionType t, CollisionData extraData) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		CollisionData getCollisionData() {
-			return null;
+			return new CollisionData(this, Parry.this);
 		}
 	}
 
@@ -48,16 +48,20 @@ public class Parry {
 			endDelay = endParry + PARRY_DELAY_DELTA;
 			switch (p.getDirection()) {
 			case DOWN:
-				hbox = new ParryHitBox(p.getX() - PARRY_LENGTH / 2, p.getY() + Player.getPlayerHeight() / 2, PARRY_LENGTH, PARRY_WIDTH, p);
+				hbox = new ParryHitBox(p.getX() - PARRY_LENGTH / 2, p.getY() + Player.getPlayerHeight() / 2,
+						PARRY_LENGTH, PARRY_WIDTH, p);
 				break;
 			case LEFT:
-				hbox = new ParryHitBox(p.getX() - Player.getPlayerWidth() / 2 - PARRY_WIDTH, p.getY() - PARRY_LENGTH / 2, PARRY_WIDTH, PARRY_LENGTH, p);
+				hbox = new ParryHitBox(p.getX() - Player.getPlayerWidth() / 2 - PARRY_WIDTH,
+						p.getY() - PARRY_LENGTH / 2, PARRY_WIDTH, PARRY_LENGTH, p);
 				break;
 			case RIGHT:
-				hbox = new ParryHitBox(p.getX() + Player.getPlayerWidth() / 2, p.getY() - PARRY_LENGTH / 2, PARRY_WIDTH, PARRY_LENGTH, p);
+				hbox = new ParryHitBox(p.getX() + Player.getPlayerWidth() / 2, p.getY() - PARRY_LENGTH / 2, PARRY_WIDTH,
+						PARRY_LENGTH, p);
 				break;
 			case UP:
-				hbox = new ParryHitBox(p.getX() - PARRY_LENGTH / 2, p.getY() - Player.getPlayerHeight() / 2 - PARRY_WIDTH, PARRY_LENGTH, PARRY_WIDTH, p);
+				hbox = new ParryHitBox(p.getX() - PARRY_LENGTH / 2,
+						p.getY() - Player.getPlayerHeight() / 2 - PARRY_WIDTH, PARRY_LENGTH, PARRY_WIDTH, p);
 				break;
 			}
 			lastY = p.getY();
