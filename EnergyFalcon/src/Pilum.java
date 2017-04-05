cad974d5970511e7e389904fcd66dd3dd660083a
denu8thell <zebrahome10@gmail.com>
2017-04-05 18:58:46 -0400

    ACTUALLY added PilumThrower.

---
+++ EnergyFalcon/src/Pilum.java
@@ -0,0 +1,131 @@
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+import arcadia.Game;
+
+public class Pilum {
+	private static final double DEFAULT_SPEED = 5.0;
+	private static int PILUM_HEIGHT = 25;
+	private static int PILUM_WIDTH = 200;
+	private static final int NUM_COLLIDERS = 15;
+	
+	private Vector2D direction;
+	private boolean shouldDelete = false;
+	private boolean shouldDestruct = false;
+	private BufferedImage pilumImage;
+	private double x, y;
+	
+	private class PilumCollider extends CircularCollision{
+		public PilumCollider(double x, double y, double r) {
+			super(x, y, r, CollisionType.ENEMY_HURTBOX_COLLISION);
+		}
+		@Override
+		void onCollide(CollisionType t, CollisionData extraData) {
+			switch(t){
+			case ENEMY_HITBOX_COLLISION:
+				break;
+			case ENEMY_HURTBOX_COLLISION:
+				break;
+			case HURTBOX_GENERAL:
+				break;
+			case PLAYER_HITBOX_COLLISION:
+				shouldDelete = true;
+				shouldDestruct = true;
+				break;
+			case PLAYER_HURTBOX_COLLISION:
+				break;
+			case WALL_COLLISION:
+				break;
+			default:
+				break;
+			
+			}
+		}
+		
+		@Override
+		CollisionData getCollisionData() {
+			return new CollisionData(this);
+		}
+	}
+	private PilumCollider[] c = new PilumCollider[NUM_COLLIDERS];
+	
+	public Pilum(double x, double y, Vector2D direction){
+		double angle = Math.atan2(direction.getY(), direction.getX());
+		this.direction = Vector2D.scale(Vector2D.unitVector(direction), DEFAULT_SPEED);
+		pilumImage = new BufferedImage(2*PILUM_WIDTH, 2*PILUM_WIDTH, BufferedImage.TYPE_4BYTE_ABGR);
+		AffineTransform t = new AffineTransform();
+		t.rotate(angle, PILUM_WIDTH, PILUM_WIDTH);
+		
+		Graphics2D g = pilumImage.createGraphics();
+		g.transform(t);
+		g.fillRect(PILUM_WIDTH, PILUM_WIDTH, PILUM_WIDTH, PILUM_HEIGHT);
+		//g.drawLine(PILUM_WIDTH, PILUM_WIDTH + PILUM_HEIGHT/2, 2*PILUM_WIDTH, PILUM_WIDTH + PILUM_HEIGHT/2);
+		
+		
+		this.x = x;
+		this.y = y;
+		for(int i = 0;i<NUM_COLLIDERS;i++){
+			int dist = i*(PILUM_WIDTH/(NUM_COLLIDERS+1)) + (PILUM_WIDTH/(NUM_COLLIDERS+1));
+			Vector2D shiftCurrent = Vector2D.rotate(new Vector2D(dist, PILUM_HEIGHT/2, 1), angle);
+			c[i] = new PilumCollider(shiftCurrent.getX()+x, shiftCurrent.getY()+y, PILUM_HEIGHT/2);
+		}
+	}
+	public Pilum(double x, double y, Vector2D direction, double speed){
+		double angle = Math.atan2(direction.getY(), direction.getX());
+		this.direction = Vector2D.scale(Vector2D.unitVector(direction), speed);
+		pilumImage = new BufferedImage(2*PILUM_WIDTH, 2*PILUM_WIDTH, BufferedImage.TYPE_4BYTE_ABGR);
+		AffineTransform t = new AffineTransform();
+		t.rotate(angle, PILUM_WIDTH, PILUM_WIDTH);
+		
+		Graphics2D g = pilumImage.createGraphics();
+		g.transform(t);
+		g.fillRect(PILUM_WIDTH, PILUM_WIDTH, PILUM_WIDTH, PILUM_HEIGHT);
+		
+		this.x = x;
+		this.y = y;
+		for(int i = 0;i<NUM_COLLIDERS;i++){
+			int dist = i*(PILUM_WIDTH/(NUM_COLLIDERS+2)) + (PILUM_WIDTH/(NUM_COLLIDERS+2));
+			Vector2D shiftCurrent = Vector2D.rotate(new Vector2D(dist, PILUM_HEIGHT/2, 1), angle);
+			c[i] = new PilumCollider(shiftCurrent.getX()+x, shiftCurrent.getY()+y, PILUM_HEIGHT/2);
+		}
+	}
+	
+	public void onTick(){
+		if(!shouldDelete){
+			for(int i = 0;i<c.length;i++){
+				c[i].setPos(c[i].getX() + direction.getX(), c[i].getY() + direction.getY());
+			}
+			x+=direction.getX();
+			y+=direction.getY();
+			//Destroy the fireball if it's out of range
+			if(c[c.length-1].getX() > Game.WIDTH || c[c.length-1].getX() < 0 || c[c.length-1].getY() > Game.HEIGHT || c[c.length-1].getY() < 0 ){
+				shouldDelete = true;
+				shouldDestruct = true;
+			}
+		}
+		if(shouldDestruct){
+			this.destruct();
+			shouldDestruct = false;
+		}
+	}
+	
+	public void draw(Graphics2D g){
+		if(!shouldDelete){
+			g.drawImage(pilumImage, (int)(x - PILUM_WIDTH), (int)(y - PILUM_WIDTH), null);
+			/*for(int i = 0;i<c.length;i++){
+				g.fillOval((int)(c[i].getX() - PILUM_HEIGHT/2), (int)(c[i].getY() - PILUM_HEIGHT/2), PILUM_HEIGHT, PILUM_HEIGHT);
+			}*/
+		}
+	}
+	
+	public boolean shouldDelete(){
+		return shouldDelete;
+	}
+	
+	public void destruct(){
+		for(int i = 0;i<c.length;i++){
+			c[i].destruct();
+		}
+	}
+}
---
+++ EnergyFalcon/src/PilumThrower.java
@@ -0,0 +1,143 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import arcadia.Input;
+
+public class PilumThrower extends Enemy{
+	private static final int ENEMY_WIDTH=75;
+	private static final int ENEMY_HEIGHT=63;
+	private static final int ENEMY_HEALTH = 3;
+	
+	private static final long FIREBALL_WAIT = 1800000000L; // Time after the wizard appears to when it throws its fireball. 1/3 seconds
+	
+	private boolean isVisible = true;
+	private boolean isEnabled = true;
+	private long fireTime = 0; 
+	private EnemyHealth health = new EnemyHealth(ENEMY_HEALTH);
+	private ArrayList<Pilum> pilums;
+	
+	private class PilumThrowerCollider extends BoxCollision{
+		public PilumThrowerCollider(double x, double y, double width, double height) {
+			super(x, y, width, height, CollisionType.ENEMY_HITBOX_COLLISION, CollisionType.ENEMY_HURTBOX_COLLISION);
+		}
+
+		void onCollide(CollisionType t, CollisionData extraData) {
+			if(isEnabled){
+				switch(t){
+				case ENEMY_HITBOX_COLLISION:
+					break;
+				case ENEMY_HURTBOX_COLLISION:
+					break;
+				case HURTBOX_GENERAL:
+					break;
+				case PLAYER_HITBOX_COLLISION:
+					break;
+				case PLAYER_HURTBOX_COLLISION:
+					if(health.canBeHurt()){
+						health.hurt();
+					}
+					break;
+				case WALL_COLLISION:
+					break;
+				default:
+					break;
+				
+				}
+			}
+		}
+
+		CollisionData getCollisionData() {
+			return new CollisionData(this, health, EnemyType.PILUM_THROWER);
+		}
+	}
+	
+	private PilumThrowerCollider c = null;
+	
+	public PilumThrower(Player p){
+		super(p, 0, 0);
+		long curTime = System.nanoTime();
+		fireTime = curTime + FIREBALL_WAIT;
+		pilums = new ArrayList<Pilum>();
+		c = new PilumThrowerCollider(x, y, ENEMY_WIDTH, ENEMY_HEIGHT);
+	}
+	
+	@Override
+	public void onTick(Input input) {
+		long curTime = System.nanoTime();
+		if(!isEnabled) return;
+		
+		c.setPos(Math.floor(x - ENEMY_WIDTH/2), Math.floor(y - ENEMY_HEIGHT/2));
+		
+		if(curTime >= fireTime){
+			//Cast fireball
+			pilums.add(new Pilum(x, y, new Vector2D(p.getX()  - x, p.getY() - y, 1)));
+			fireTime = curTime + FIREBALL_WAIT;
+		}
+		//Update all fireballs, delete ones marked for deletion.
+		Iterator<Pilum> itPilum = pilums.iterator();
+		Pilum f;
+		while(itPilum.hasNext()){
+			f = itPilum.next();
+			f.onTick();
+			if(f.shouldDelete()){
+				itPilum.remove();
+			}
+		}
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		if(isVisible){
+			g.setColor(Color.CYAN);
+			g.fillRect((int)Math.floor(x - ENEMY_WIDTH/2), (int)Math.floor(y - ENEMY_HEIGHT/2), (int)(ENEMY_WIDTH), (int)(ENEMY_HEIGHT));
+		}
+		g.setColor(Color.red);
+		for(Pilum f : pilums){
+			f.draw(g);
+		}
+	}
+
+	@Override
+	public Collider getCollider() {
+		return c;
+	}
+
+	@Override
+	public boolean isDead() {
+		return health.getEnemyHealth() <= 0;
+	}
+
+	@Override
+	public void destruct() {
+		c.destruct();
+		Iterator<Pilum> itPilum = pilums.iterator();
+		Pilum f;
+		while(itPilum.hasNext()){
+			f = itPilum.next();
+			f.destruct();
+			itPilum.remove();
+		}
+	}
+
+	@Override
+	public void disableEnemy() {
+		isEnabled = false;
+	}
+
+	@Override
+	public void enableEnemy() {
+		isEnabled = true;
+	}
+
+	@Override
+	public double getWidth() {
+		return ENEMY_WIDTH;
+	}
+
+	@Override
+	public double getHeight() {
+		return ENEMY_HEIGHT;
+	}
+}
